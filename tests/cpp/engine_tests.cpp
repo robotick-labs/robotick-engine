b0b032edcd690641701be20fb8e652502286c4bc
paulwconnor <paulwconnor@hotmail.com>
2025-05-13 20:17:16 +0100

    Engine - got unit-tests working

--- tests/cpp/engine_tests.cpp
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Sample test works") {
-    REQUIRE(1 + 1 == 2);
-}
---
+++ tests/cpp/framework/Engine.test.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+#include "robotick/framework/Engine.h"
+#include "robotick/framework/Model.h"
+#include "robotick/framework/WorkloadBase.h"
+
+using namespace robotick;
+
+class CountingWorkload : public WorkloadBase {
+public:
+    int tick_count = 0;
+
+    CountingWorkload(std::string name, double hz)
+        : WorkloadBase(std::move(name), hz) {}
+
+    void tick(const InputBlock&, OutputBlock&) override {
+        ++tick_count;
+    }
+};
+
+TEST_CASE("Engine runs workloads at expected rate (approx)") {
+    Model model;
+    auto w = std::make_shared<CountingWorkload>("counter", 10.0);
+    model.add(w);
+
+    Engine engine;
+    engine.load(model);
+    engine.setup();
+    engine.start();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(120));
+    engine.stop();
+
+    REQUIRE(w->tick_count >= 1);  // Should tick at least once
+}
