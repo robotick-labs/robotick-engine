b12e5589add184ec4d83cabff5fd49691b44d973
paulwconnor <paulwconnor@hotmail.com>
2025-05-17 21:09:56 +0000

    git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -1,2 +1,2 @@
-echo "Setting URL Origin to: https://github.com/robotick/robotick.git"
-git remote set-url origin https://github.com/robotick/robotick.git
+echo "Setting URL Origin to: https://github.com/robotick-labs/robotick.git"
+git remote set-url origin https://github.com/robotick-labs/robotick.git
