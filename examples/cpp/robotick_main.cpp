d6b97e5bd961e3636cf04f83d3384724b244a8a8
paulwconnor <paulwconnor@hotmail.com>
2025-05-17 15:53:39 +0000

    #27 Improving core interfaces (allow F5 to be used to run again)

--- examples/cpp/robotick_main.cpp
+++ examples/cpp/robotick_main.cpp
@@ -18,46 +18,40 @@ void signal_handler(int)
     g_exit_requested = true;
 }
 
-#if 0
-void populate_model(Model &model)
-{
-    model.add("PythonWorkload", "py",
-                      {{"script_name", "robotick.workloads.optional.test.hello_workload"},
-                       {"class_name", "HelloWorkload"}});
-}
-#else
 void populate_model(Model& model)
 {
     model.add("TimingDiagnosticsWorkload", "timing_diagnostics", 1000.0, {{"report_every", 1000}});
 
-    if (true) return;
+    if (true)
+    {
+        return;
+    }
 
     // === Add top-level standalone workloads ===
     model.add("PythonWorkload", "comms", 30.0,
-                       {{"script_name", "robotick.workloads.core.telemetry.mqtt_update"}, {"class_name", "MqttUpdate"}});
+              {{"script_name", "robotick.workloads.core.telemetry.mqtt_update"}, {"class_name", "MqttUpdate"}});
 
     model.add("PythonWorkload", "console", 2.0, {{"script_name", "console_update"}, {"class_name", "ConsoleUpdate"}});
 
     // === Subcomponents for synced pair ===
-    auto brickpi = model.add("PythonWorkload", "brickpi3_interface", 100.0,
-                                      {{"script_name", "brickpi"}, {"class_name", "BrickPi3Interface"}});
+    auto brickpi =
+        model.add("PythonWorkload", "brickpi3_interface", 100.0, {{"script_name", "brickpi"}, {"class_name", "BrickPi3Interface"}});
 
-    auto remote = model.add("PythonWorkload", "remote_control", 30.0,
-                                     {{"script_name", "remote"}, {"class_name", "RemoteControlInterface"}});
+    auto remote =
+        model.add("PythonWorkload", "remote_control", 30.0, {{"script_name", "remote"}, {"class_name", "RemoteControlInterface"}});
 
     auto deadzone = model.add("PythonWorkload", "deadzone_transformer", 30.0,
-                                       {{"script_name", "transform"}, {"class_name", "DeadZoneScaleAndSplitTransformer"}});
+                              {{"script_name", "transform"}, {"class_name", "DeadZoneScaleAndSplitTransformer"}});
 
     auto mixer = model.add("PythonWorkload", "steering_mixer", 30.0,
-                                    {{"script_name", "steering"}, {"class_name", "SteeringMixerTransformer"}});
+                           {{"script_name", "steering"}, {"class_name", "SteeringMixerTransformer"}});
 
     // === Sequence and Pair ===
     auto sequence = model.add("SequenceWorkload", "control_sequence", 0.0,
-                                       {{"children", std::vector<WorkloadHandle>{remote, deadzone, mixer}}});
+                              {{"children", std::vector<WorkloadHandle>{remote, deadzone, mixer}}});
 
     model.add("SyncedPairWorkload", "control_pair", 100.0, {{"primary", brickpi}, {"secondary", sequence}});
 }
-#endif  // #if 1
 
 int main()
 {
@@ -66,8 +60,7 @@ int main()
 
     Model model;
     populate_model(model);
-    model.finalise();  // finalise memory - creating actual workloads in a single
-                       // large buffer
+    model.finalise();  // finalise memory - creating actual workloads in a single large buffer
 
     Engine engine;
     engine.load(model);
