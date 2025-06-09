f23316b3bd37186da6e227b7527947f3e0dc60ad
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-06-09 12:30:01 +0000

    [#130] added initial Model::add_remote_model() code; and moved model.h inlines to cpp unless trivial

--- cpp/include/robotick/framework/Model.h
+++ cpp/include/robotick/framework/Model.h
@@ -17,10 +17,21 @@
 
 namespace robotick
 {
+	struct RemoteModelSeed;
+
 	struct WorkloadHandle
 	{
 		uint32_t index = uint32_t(-1); // Indicates invalid/unset by default
 		bool is_valid() const { return index != uint32_t(-1); }
+	};
+
+	struct WorkloadSeed
+	{
+		std::string type;
+		std::string name;
+		double tick_rate_hz;
+		std::vector<WorkloadHandle> children;
+		std::map<std::string, std::string> config;
 	};
 
 	class Model
@@ -28,52 +39,17 @@ namespace robotick
 	  public:
 		static constexpr double TICK_RATE_FROM_PARENT = 0.0;
 
-		struct WorkloadSeed
-		{
-			std::string type;
-			std::string name;
-			double tick_rate_hz;
-			std::vector<WorkloadHandle> children;
-			std::map<std::string, std::string> config;
-		};
-
 		WorkloadHandle add(const std::string& type, const std::string& name, double tick_rate_hz = TICK_RATE_FROM_PARENT,
-			const std::map<std::string, std::string>& config = {})
-		{
-			if (root_workload.is_valid())
-				ROBOTICK_FATAL_EXIT("Cannot add workloads after root has been set. Model root must be set last.");
-
-			const std::vector<WorkloadHandle> children = {};
-			workload_seeds.push_back({type, name, tick_rate_hz, children, config});
-			return {static_cast<uint32_t>(workload_seeds.size() - 1)};
-		}
+			const std::map<std::string, std::string>& config = {});
 
 		WorkloadHandle add(const std::string& type, const std::string& name, const std::vector<WorkloadHandle>& children,
-			double tick_rate_hz = TICK_RATE_FROM_PARENT, const std::map<std::string, std::string>& config = {})
-		{
-			if (root_workload.is_valid())
-				ROBOTICK_FATAL_EXIT("Cannot add workloads after root has been set. Model root must be set last.");
-
-			for (auto child : children)
-			{
-				if (!child.is_valid() || child.index >= workload_seeds.size())
-					ROBOTICK_FATAL_EXIT("Child handle out of range when adding workload '%s'", name.c_str());
-			}
-
-			workload_seeds.push_back({type, name, tick_rate_hz, children, config});
-			return {static_cast<uint32_t>(workload_seeds.size() - 1)};
-		}
+			double tick_rate_hz = TICK_RATE_FROM_PARENT, const std::map<std::string, std::string>& config = {});
 
 		void connect(const std::string& source_field_path, const std::string& dest_field_path);
 
-		void set_root(WorkloadHandle handle, const bool auto_finalize = true)
-		{
-			root_workload = handle; // no more changes once root has been set, so good time to validate the model...
-			if (auto_finalize)
-			{
-				finalize();
-			}
-		}
+		void add_remote_model(const Model& remote_model, const std::string& model_name, const std::string& comms_channel);
+
+		void set_root(WorkloadHandle handle, const bool auto_finalize = true);
 
 		const std::vector<WorkloadSeed>& get_workload_seeds() const { return workload_seeds; }
 
@@ -86,6 +62,17 @@ namespace robotick
 	  private:
 		std::vector<WorkloadSeed> workload_seeds;
 		std::vector<DataConnectionSeed> data_connection_seeds;
+
+		std::unordered_map<std::string, RemoteModelSeed> remote_models;
+
 		WorkloadHandle root_workload; // <- Root workload entry point
 	};
+
+	struct RemoteModelSeed
+	{
+		std::string model_name;
+		std::string comms_channel; // e.g. "uart:/dev/ttyUSB0", "ip:192.168.1.42", "local"
+		Model model;
+	};
+
 } // namespace robotick
