36d25246a72fc9c1af9facb0d9dd5c1d81c619cb
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-07-23 14:13:02 +0100

    Docker fix

--- docker-build-ubuntu.sh
+++ docker-build-ubuntu.sh
@@ -2,4 +2,4 @@
 set -e
 
 SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-docker build -t robotick-dev:linux -f "$SCRIPT_DIR/docker/Docker.linux.dev" .
+docker build -t robotick-dev:linux -f "$SCRIPT_DIR/docker/Docker.ubuntu.dev" .
