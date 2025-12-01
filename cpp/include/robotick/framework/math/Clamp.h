a3abe6476effd2a7f9d45b50a8bbb5b1049c4c22
paulwconnor-ai <paulwconnor.ai@outlook.com>
2025-12-01 10:15:59 +0000

    ...

--- cpp/include/robotick/framework/math/Clamp.h
+++ cpp/include/robotick/framework/math/Clamp.h
@@ -5,16 +5,6 @@
 
 namespace robotick
 {
-	template <typename T> inline const T& min_value(const T& a, const T& b)
-	{
-		return (a < b) ? a : b;
-	}
-
-	template <typename T> inline const T& max_value(const T& a, const T& b)
-	{
-		return (a > b) ? a : b;
-	}
-
 	template <typename T> inline T clamp(T value, const T& lo, const T& hi)
 	{
 		if (value < lo)
